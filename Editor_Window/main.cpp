776911d08251ee7a84ecc1c373289ea23388e537
Mr.Han <h920915@gmail.com>
2024-06-17 03:11:01 +0900

    정적라이브러리

--- Editor_Window/main.cpp
+++ Editor_Window/main.cpp
@@ -3,6 +3,9 @@
 
 #include "framework.h"
 #include "Editor_Window.h"
+#include "../JYEngine_SOURCE/JYApplication.h"
+
+Application app;
 
 #define MAX_LOADSTRING 100
 
@@ -28,6 +31,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // TODO: 여기에 코드를 입력합니다.
+    app.test();
 
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
---
+++ JYEngine_SOURCE/JYApplication.cpp
@@ -0,0 +1,6 @@
+#include "JYApplication.h"
+
+void Application::test()
+{
+	int a = 0;
+}
